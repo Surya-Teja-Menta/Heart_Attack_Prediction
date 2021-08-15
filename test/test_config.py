2fe3fb5c4497bea6afda7233ba06295a01266770
Surya <mentasuryateja@gmail.com>
2021-08-15 22:20:52 +0530

    Updated

--- test/test_config.py
+++ test/test_config.py
@@ -1,3 +1,17 @@
+import pytest
+
+class NotInRange(Exception):
+    def __init__(self,message="Value not valid"):
+        self.message=message
+        super().__init__(self.message)
+
+
+
+
+
+
+
+
 def test_generic():
     a=42
     b=42

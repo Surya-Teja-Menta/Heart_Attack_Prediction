b349face7fdda42744527357b0835e0c8da7db0e
Surya <mentasuryateja@gmail.com>
2021-08-15 22:07:19 +0530

    Updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(
+    name='src',
+    version='0.0.1',
+    description='Its a Heart Attack application package',
+    author='Surya Teja Menta',
+    packages=find_packages(),
+    licence='MIT'
+)
---
+++ test/test_config.py
@@ -0,0 +1,4 @@
+def test_generic():
+    a=42
+    b=42
+    assert a==b

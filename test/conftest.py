eccdff2c6a830c5055d0ec91029e1e08add76c02
Surya <mentasuryateja@gmail.com>
2021-08-18 20:34:01 +0530

    Updated Successfully

---
+++ test/conftest.py
@@ -0,0 +1,9 @@
+import yaml,os,pytest,json
+import prediction_service
+@pytest.fixture
+
+def config(config_path='params.yaml'):
+    with open(config_path) as file:
+        config=yaml.load(file)
+    return config
+
